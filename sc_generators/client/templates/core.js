debe0d4b662d95e2880d1a0e85320d4c960c438a
Charles Jolley <charles@sproutit.com>
2008-06-24 11:29:49 -0700

    Removed unused controllers array from core.js template

--- sc_generators/client/templates/core.js
+++ sc_generators/client/templates/core.js
@@ -11,9 +11,6 @@
   // When you are in development mode, this array will be populated with 
   // any fixtures you create for testing and loaded automatically in your
   // main method.  When in production, this will be an empty array.
-  FIXTURES: [],
-
-  // Any keys in this array will be instantiated automatically from main.
-  controllers: []
+  FIXTURES: []
   
 }) ;
