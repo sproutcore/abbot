98831e524773fd708d8f23dcf28be5d1d364042a
Glenn Rempe <glenn@rempe.us>
2008-06-25 15:27:11 -0700

    Whitespace

--- app_generators/sproutcore/sproutcore_generator.rb
+++ app_generators/sproutcore/sproutcore_generator.rb
@@ -1,14 +1,14 @@
 require 'sproutcore/generator_helper'
 
 class SproutcoreGenerator < RubiGen::Base
-  
+
   DEFAULT_SHEBANG = File.join(Config::CONFIG['bindir'],
                               Config::CONFIG['ruby_install_name'])
-  
+
   default_options :author => nil
-  
+
   attr_reader :name
-  
+
   def initialize(runtime_args, runtime_options = {})
     super
     usage if args.empty?
@@ -22,16 +22,16 @@ class SproutcoreGenerator < RubiGen::Base
       # Ensure appropriate folder(s) exists
       m.directory ''
       BASEDIRS.each { |path| m.directory path }
-      
+
       # Create stubs
       m.template_copy_each %w(README sc-config.rb)
-      # m.dependency "install_rubigen_scripts", [destination_root, 'sproutcore'], 
+      # m.dependency "install_rubigen_scripts", [destination_root, 'sproutcore'],
       #   :shebang => options[:shebang], :collision => :force
-        
+
       # Create initial client
-      m.dependency "client", [@name, "--library=#{@destination_root}"], 
+      m.dependency "client", [@name, "--library=#{@destination_root}"],
         :shebang => options[:shebang], :collision => :force
-        
+
     end
   end
 
@@ -54,7 +54,7 @@ EOS
       #         "Default: none") { |options[:author]| }
       opts.on("-v", "--version", "Show the #{File.basename($0)} version number and quit.")
     end
-    
+
     def extract_options
       # for each option, extract it into a local variable (and create an "attr_reader :author" at the top)
       # Templates can access these value via the attr_reader-generated methods, but not the
