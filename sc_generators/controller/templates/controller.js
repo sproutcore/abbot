be459f4bb0334c3f8f702c664696db7a9330d95b
Glenn Rempe <glenn@rempe.us>
2008-06-25 14:58:25 -0700

    Remove extra whitespace before authorname.  General whitespace cleanup.

--- sc_generators/controller/templates/controller.js
+++ sc_generators/controller/templates/controller.js
@@ -9,13 +9,13 @@ require('core');
   (Document Your View Here)
 
   @extends <%= base_class_name 'SC.Object' %>
-  @author    AuthorName  
+  @author AuthorName
   @version 0.1
   @static
 */
 <%= controller_instance_name %> = <%= base_class_name 'SC.Object' %>.create(
 /** @scope <%= controller_instance_name %> */ {
-  
+
   // TODO: Add your own code here.
-  
+
 }) ;
