19a032a6bc49d2c2eafd60014c9418467ee5cc0b
Charles Jolley <charles@sproutit.com>
2009-01-08 23:36:13 -0800

    Cleanup most unused code

--- spec/abbot/manifest/prepare_spec.rb
+++ spec/abbot/manifest/prepare_spec.rb
@@ -3,8 +3,5 @@ require File.join(File.dirname(__FILE__), %w[.. .. spec_helper])
 describe Abbot::Manifest, 'prepare!' do
   
   include Abbot::SpecHelpers
-
-  it "should execute the manifest:prepare filter" do
-  end
   
 end
