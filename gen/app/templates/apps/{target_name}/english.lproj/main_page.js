0dbf71068a4ed27c67c6cd17e3fd5bf6b2bece9b
Charles Jolley <charles@sproutit.com>
2009-04-05 10:21:48 -0700

    Update template to use latest API

--- gen/app/templates/apps/{target_name}/english.lproj/main_page.js
+++ gen/app/templates/apps/{target_name}/english.lproj/main_page.js
@@ -10,9 +10,13 @@
   // The main pane is made visible on screen as soon as your app is loaded.
   // Add childViews to this pane for views to display immediately on page 
   // load.
-  mainPane: SC.MainPane.design()
-    .childView(SC.LabelView.design({
+  mainPane: SC.MainPane.design({
+    childViews: 'labelView'.w(),
+    
+    labelView: SC.LabelView.design({
+      layout: { centerX: 0, centerY: 0, width: 100, height: 18 },
       tagName: "h1", value: "Hello World"
-    }))
+    })
+  })
 
 });
