cf8f4c2afe9821a3fe256092bfd6c951cfc5c6d5
Charles Jolley <charles@sproutit.com>
2009-07-27 02:39:06 -0700

    Add generator for page design

---
+++ gen/design/templates/english.lproj/@filename@.js
@@ -0,0 +1,16 @@
+// ==========================================================================
+// Project:   <%= namespace_instance_name %>
+// Copyright: ©<%= Time.now.year %> My Company, Inc.
+// ==========================================================================
+/*globals <%= namespace %> */
+
+// This page describes a part of the interface for your application.
+<%= namespace_instance_name %> = SC.Page.design({
+
+  // Add your views here.  Ex:
+  
+  // mainView: SC.View.design({
+  //   layout: { top: 0, left: 0, right: 0, height: 0 }
+  // })
+
+});
