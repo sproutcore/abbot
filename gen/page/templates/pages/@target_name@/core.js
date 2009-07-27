61ded3eb41bf69c0438e94d7e77596a7eade49ad
Charles Jolley <charles@sproutit.com>
2009-07-27 02:38:57 -0700

    Add generator for static pages

---
+++ gen/page/templates/pages/@target_name@/core.js
@@ -0,0 +1,22 @@
+// ==========================================================================
+// Project:   <%= namespace %>
+// Copyright: ©<%= Time.now.year %> My Company, Inc.
+// ==========================================================================
+/*globals <%= namespace %> */
+
+/** @namespace
+
+  Static page JavaScript
+  
+  @extends SC.Object
+*/
+<%= namespace %> = SC.Object.create(
+  /** @scope <%= namespace %>.prototype */ {
+
+  NAMESPACE: '<%= namespace %>',
+  VERSION: '0.1.0'
+  
+  // TODO: You can place any JavaScript you need on your page here.  If you
+  // don't want any JavaScript, just delete this file.
+
+}) ;
