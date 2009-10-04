11d2779f0ba311dfe6265dba00322fc1117e648a
Charles Jolley <charles@sproutit.com>
2009-10-04 14:15:37 -0700

    Add generator for data-source

---
+++ gen/data-source/templates/data_sources/@filename@.js
@@ -0,0 +1,64 @@
+// ==========================================================================
+// Project:   <%= namespace_class_name %>
+// Copyright: ©<%= Time.now.year %> My Company, Inc.
+// ==========================================================================
+/*globals <%= namespace %> */
+
+/** @class
+
+  (Document Your Data Source Here)
+
+  @extends <%= base_class_name || 'SC.DataSource' %>
+*/
+<%= namespace_class_name %> = <%= base_class_name || 'SC.DataSource' %>.create(
+/** @scope <%= namespace_class_name %>.prototype */ {
+
+  // ..........................................................
+  // QUERY SUPPORT
+  // 
+
+  fetch: function(store, query) {
+
+    // TODO: Add handlers to fetch data for specific queries.  
+    // call store.dataSourceDidFetchQuery(query) when done.
+
+    return NO ; // return YES if you handled the query
+  },
+
+  // ..........................................................
+  // RECORD SUPPORT
+  // 
+  
+  retrieveRecord: function(store, storeKey) {
+    
+    // TODO: Add handlers to retrieve an individual record's contents
+    // call store.dataSourceDidComplete(storeKey) when done.
+    
+    return NO ; // return YES if you handled the storeKey
+  },
+  
+  createRecord: function(store, storeKey) {
+    
+    // TODO: Add handlers to submit new records to the data source.
+    // call store.dataSourceDidComplete(storeKey) when done.
+    
+    return NO ; // return YES if you handled the storeKey
+  },
+  
+  updateRecord: function(store, storeKey) {
+    
+    // TODO: Add handlers to submit modified record to the data source
+    // call store.dataSourceDidComplete(storeKey) when done.
+
+    return NO ; // return YES if you handled the storeKey
+  },
+  
+  destroyRecord: function(store, storeKey) {
+    
+    // TODO: Add handlers to destroy records on the data source.
+    // call store.dataSourceDidDestroy(storeKey) when done
+    
+    return NO ; // return YES if you handled the storeKey
+  }
+  
+}) ;
