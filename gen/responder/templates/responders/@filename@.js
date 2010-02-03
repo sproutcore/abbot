dcf434c72edb72f67beb939325effdfb5f1afe00
Colin Campbell <colin.campbell@gmail.com>
2010-02-03 11:39:24 -0330

    Added sc-gen template for SC.Responder

---
+++ gen/responder/templates/responders/@filename@.js
@@ -0,0 +1,19 @@
+// ==========================================================================
+// Project:   <%= namespace %>.<%= class_name.upcase %>
+// Copyright: ©<%= Time.now.year %> My Company, Inc.
+// ==========================================================================
+/*globals <%= namespace %> */
+
+/** @class
+
+  (Document your Responder here)
+
+  @extends <%= base_class_name || 'SC.Responder' %>
+  @version 0.1
+*/
+<%= namespace %>.<%= class_name.upcase %> = <%= base_class_name || 'SC.Responder' %>.create(
+/** @scope <%= namespace %>.<%= class_name.upcase %>.prototype */ {
+
+  // TODO: Add your own code here.
+
+}) ;
