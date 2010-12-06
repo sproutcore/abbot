f5d3622ea914de96276be967de10923974bf7031
Alex Iskander <alexiskander@me.com>
2010-12-05 23:51:55 -0600

    Clean up a little.

--- vendor/chance/lib/chance/imagers/data_url.rb
+++ vendor/chance/lib/chance/imagers/data_url.rb
@@ -15,8 +15,7 @@ module Chance
         output += "." + slice[:css_name] + " { "
         output += "background-image: url("
         output += '"data:image/png;base64,'
-
-        # So now, if we are actually slicing, we nee
+        
         base64Image = Base64.encode64(slice[:image].to_blob({ :fast_rgba => true }))
         output += base64Image
 
@@ -27,6 +26,7 @@ module Chance
 
       return output
     end
+    
   end
 
 end
