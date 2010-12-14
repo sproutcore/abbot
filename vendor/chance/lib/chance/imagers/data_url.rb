db9a276791faf52368343f8c832422a66e4d71b3
Alex Iskander <alexiskander@me.com>
2010-12-14 12:07:03 -0600

    Actually use :fast_rgba

--- vendor/chance/lib/chance/imagers/data_url.rb
+++ vendor/chance/lib/chance/imagers/data_url.rb
@@ -6,18 +6,19 @@ module Chance
 
   # The DataURL Imager creates CSS with data urls for each slice.
   class DataURLImager < Chance::Imager
-
+    
     def css
       output = ""
       slices = @slices
-
+      
       slices.each do |name, slice|
         # so, the path should be the path in the chance instance
         output += "." + slice[:css_name] + " { "
         output += "background: url("
         output += '"data:image/png;base64,'
         
-        base64Image = Base64.encode64(slice[:image].to_blob({ :fast_rgba => true }))
+        base64Image = Base64.encode64(slice[:image].to_blob(:fast_rgba))
+        
         output += base64Image
 
         output += '"'
