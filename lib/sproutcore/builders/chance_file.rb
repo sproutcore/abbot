e2b2c82f52d6aba5a41d43c851f74bf5ef0de408
Alex Iskander <alexiskander@me.com>
2011-01-22 11:28:25 -0600

    Make a generic chance file handler

--- lib/sproutcore/builders/chance_file.rb
+++ lib/sproutcore/builders/chance_file.rb
@@ -4,23 +4,25 @@ require "chance"
 
 module SC
   # This builder builds CSS using Chance.
-  class Builder::ChanceJavaScript < Builder::Base
+  class Builder::ChanceFile < Builder::Base
 
     def build(dst_path)
       entries = entry.ordered_entries || entry.source_entries
+      chance_file = entry[:chance_file]
 
-      javascript = entries.map {|entry|
+      src = entries.map {|entry|
         chance = entry[:chance]
         src = ""
-        src = chance.files["chance.js"] unless chance.nil?
+        src = chance.files[chance_file] unless chance.nil?
 
         src
       }.join("\n")
 
-      writeline dst_path, javascript
+      writeline dst_path, src
     end
 
   end
 
 end
+
 
