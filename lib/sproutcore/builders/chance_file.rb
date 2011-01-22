21e4b4f3d84306eecd400657a5da66863e4ad2b2
Alex Iskander <alexiskander@me.com>
2011-01-22 15:44:10 -0600

    ChanceFile should handle static_url

--- lib/sproutcore/builders/chance_file.rb
+++ lib/sproutcore/builders/chance_file.rb
@@ -12,14 +12,31 @@ module SC
 
       src = entries.map {|entry|
         chance = entry[:chance]
+
         src = ""
         src = chance.files[chance_file] unless chance.nil?
+        src = "" if src.nil?
+
+        src = rewrite_inline_code(src) if chance_file.end_with?("css")
 
         src
       }.join("\n")
 
       writeline dst_path, src
     end
+
+    # Rewrites any inline content such as static urls.  Subclasseses can
+    # override this to rewrite any other inline content.
+    #
+    # The default will rewrite calls to static_url().
+    def rewrite_inline_code(code)
+      # look for sc_require, require or sc_resource.  wrap in comment
+      code.gsub!(/((sc_require|require|sc_resource)\(\s*['"].*["']\s*\)\s*\;)/, '/* \1 */')
+      replace_static_url(code)
+      code
+    end
+
+    def static_url(url=''); "url('#{url}')" ; end
 
   end
 
