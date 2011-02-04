50c561f28df6ff1ac41253e6f6c29af1ba6556be
Alex Iskander <alexiskander@me.com>
2011-02-03 18:16:01 -0600

    Add some basic tests for Chance-Abbot integration. Still need to test combining of @2x and a few other things on the manifest level.

---
+++ spec/buildtasks/manifest/prepare_build_tasks/chance_spec.rb
@@ -0,0 +1,100 @@
+require "buildtasks/manifest/spec_helper"
+
+describe "manifest:prepare_build_tasks:chance" do
+  include SC::SpecHelpers
+  include SC::ManifestSpecHelpers
+
+  before do
+    std_before
+  end
+
+  def run_task
+    @manifest.prepare!
+    super('manifest:prepare_build_tasks:chance')
+  end
+
+  it "should run manifest:prepare_build_tasks:setup as prereq" do
+    should_run('manifest:prepare_build_tasks:setup') { run_task }
+  end
+
+
+  def have_entry(name)
+    entry = @manifest.entry_for name
+    entry.should_not be_nil
+  end
+
+  it "generates @2x for each resource" do
+    run_task
+
+    resources = {}
+
+    originals = @manifest.entries(:hidden=>true).select {|entry|
+      entry.entry_type == :css and not entry.combined and not entry[:resource].nil?
+    }
+    originals.size.should > 0
+
+    originals.each do |entry|
+      next if entry[:resource].nil?
+
+      resources[entry[:resource]] ||= []
+      resources[entry[:resource]] << entry
+    end
+
+    resources.each do |name, entries|
+      have_entry(name + ".css")
+      have_entry(name + "@2x.css")
+
+      # check that transforms are created
+      entries.each {|entry|
+        entry.should be_transform
+        entry.source_entry.should_not be_nil
+        originals.should include(entry)
+        originals.delete(entry)
+      }
+
+      x2_entry = entry_for(name + "@2x.css")
+      x2_entry[:chance_file].should == "chance@2x.css"
+      x2_entry[:chance_entry].should == entry_for(name + ".css")
+    end
+    originals.size.should == 0
+  end
+
+  it "generates js, and mhtml entries" do
+    run_task
+
+    have_entry('stylesheet.css')
+    have_entry('stylesheet@2x.css')
+    have_entry('__sc_chance.js')
+    have_entry('__sc_chance_mhtml.txt')
+
+    stylesheet = entry_for('stylesheet.css')
+  end
+
+  it "provides all chance entries to the mhtml and js entries" do
+    run_task
+
+    js_entry = entry_for('__sc_chance.js')
+    mhtml_entry = entry_for('__sc_chance_mhtml.txt')
+
+    resources = {}
+
+    originals = @manifest.entries(:hidden=>true).select {|entry|
+      entry.entry_type == :css and not entry.combined and not entry[:resource].nil?
+    }
+    originals.size.should > 0
+
+    originals.each do |entry|
+      next if entry[:resource].nil?
+
+      resources[entry[:resource]] ||= []
+      resources[entry[:resource]] << entry
+    end
+
+    resources.each do |name, entries|
+      chance_entry = entry_for(name + ".css")
+      js_entry.chance_entries.should include(chance_entry)
+      mhtml_entry.chance_entries.should include(chance_entry)
+    end
+
+  end
+end
