122f624630797e805339fb14a1e656fbc0ffe360
Alex Iskander <alexiskander@me.com>
2011-02-04 14:48:25 -0600

    Add fixture for Chance

---
+++ spec/fixtures/builder_tests/apps/chance_test/resources/main_page.js
@@ -0,0 +1,24 @@
+// ==========================================================================
+// Project:   ChanceTest - mainPage
+// Copyright: ©2011 My Company, Inc.
+// ==========================================================================
+/*globals ChanceTest */
+
+// This page describes the main user interface for your application.  
+ChanceTest.mainPage = SC.Page.design({
+
+  // The main pane is made visible on screen as soon as your app is loaded.
+  // Add childViews to this pane for views to display immediately on page 
+  // load.
+  mainPane: SC.MainPane.design({
+    childViews: 'labelView'.w(),
+    
+    labelView: SC.LabelView.design({
+      layout: { centerX: 0, centerY: 0, width: 200, height: 18 },
+      textAlign: SC.ALIGN_CENTER,
+      tagName: "h1", 
+      value: "Welcome to SproutCore!"
+    })
+  })
+
+});
