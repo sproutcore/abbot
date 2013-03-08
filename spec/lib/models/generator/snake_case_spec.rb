4b910c238c9060122d4175b652f6e5bda98c1abd
Ian James <ianejames@yahoo.com>
2013-03-08 11:14:55 -0800

    Adds spec for SC::Generator#snake_case

---
+++ spec/lib/models/generator/snake_case_spec.rb
@@ -0,0 +1,25 @@
+require "spec_helper"
+
+describe SC::Generator, 'snake_case' do
+
+  test_hashes = [
+    { :input => "FooBar", :output => "foo_bar" },
+    { :input => "HeadlineCNNNews", :output => "headline_cnn_news" },
+    { :input => "CNN", :output => "cnn" },
+    { :input => "innerHTML", :output => "inner_html" },
+    { :input => "Foo_Bar", :output => "foo_bar" },
+    { :input => "Foo-Bar", :output => "foo_bar" },
+  ]
+
+  test_hashes.each do |test_hash|
+    input = test_hash[:input]
+    output = test_hash[:output]
+
+    it "should snake_case #{input} to #{output}" do
+      a = SC::Generator.new("test")
+
+      a.snake_case(input).should eql(output)
+    end
+  end
+
+end
